8e60fdc950166552ad42d7cf9b4a9fff66a2c11e
Alexandr-sys2024 <time2016slot@gmail.com>
2024-10-17 00:58:34 +0300

    add  new code

--- main.py
+++ main.py
@@ -5,24 +5,13 @@ from selenium.webdriver.common.by import By
 import time
 
 browser = webdriver.Chrome()
-#browser.get("https://en.wikipedia.org/wiki/Document_Object_Model")
-#browser.save_screenshot("dom.png")
-#time.sleep(5)
-#browser.quit()
-#browser.get("https://ru.wikipedia.org/wiki/Selenium")
-#browser.save_screenshot("selenium.png")
-#time.sleep(3)
-#browser.refresh()
-browser.get("https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0")
-#Проверяем по заголовку, тот ли сайт открылся
-assert "Википедия" in browser.title
-time.sleep(5)
-#Находим окно поиска
-search_box = browser.find.element(By.ID, "searchInput")
-#Прописываем ввод текста в поисковую строку. В кавычках тот текст, который нужно ввести
-search_box.send_keys("Солнечная система")
-#Добавляем не только введение текста, но и его отправку
-search_box.send_keys(Keys.RETURN)
-time.sleep(5)
+browser.get("https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D0%BB%D0%BD%D0%B5%D1%87%D0%BD%D0%B0%D1%8F_%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D0%B0")
+
+paragraphs = browser.find_elements(By.TAG_NAME, "p")
+#Для перебора пишем цикл
+for paragraph in paragraphs:
+
+    print(paragraph.text)
+    input()
 
 
