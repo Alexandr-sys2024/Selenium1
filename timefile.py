2082b74c8a83928eab2e366502ee830a0403c5dd
Alexandr-sys2024 <time2016slot@gmail.com>
2024-10-18 13:32:44 +0300

    add new file

--- timefile.py
+++ timefile.py
@@ -1,41 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+import requests
 import time
 
+browser = webdriver.Yandex()
+browser.get("https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0")
+
+assert  "Википедия" in browser.title
+time.sleep(5)
+search_box = browser.find_element(By.ID, "searchInput")
+search_box.send_keys("Солнечная система")
 
 
-
-#paragraphs = browser.find_elements(By.TAG_NAME, "p")
-
-#for paragraph in paragraphs:
-#    print(paragraph.text)
-#    input()
-
-#assert "Википедия" in browser.title
-#time.sleep(5)
-
-#Находим окно поиска
-#search_box = browser.find.element(By.ID, "searchInput")
-#search_box.send_keys(Keys.RETURN)
-#time.sleep(5)
-#a = browser.find_element(By.LINK_TEXT, "Солнечная система")
-#Добавляем клик на элемент
-#a.click()
-
-
-#browser.get("https://en.wikipedia.org/wiki/Document_Object_Model")
-#browser.save_screenshot("dom.png")
-#time.sleep(5)
-#browser.quit()
-#browser.get("https://ru.wikipedia.org/wiki/Selenium")
-#browser.save_screenshot("selenium.png")
-#time.sleep(3)
-#browser.refresh()
-#Проверяем по заголовку, тот ли сайт открылся
-
-#Находим окно поиска
-#search_box = browser.find.element(By.ID, "searchInput")
-#Прописываем ввод текста в поисковую строку. В кавычках тот текст, который нужно ввести
-#search_box.send_keys("Солнечная система Расширенный поиск")
-#Добавляем не только введение текста, но и его отправку
-#search_box.send_keys(Keys.RETURN)
-#time.sleep(5)
